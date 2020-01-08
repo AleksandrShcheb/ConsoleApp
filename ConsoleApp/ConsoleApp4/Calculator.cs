dfd7dcd90a8ad0203a7f38ad71782128acb96405
Aleksandr <danserdgit@yandex.ru>
2020-01-08 07:43:25 +0300

    new static example

---
+++ ConsoleApp/ConsoleApp4/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public static class Calculator
+    {
+        public static int Sum (int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum (double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Averange(params double[] numbers)
+        {
+            double result = 0;
+            foreach (var item in numbers)
+            {
+                result += item;
+            }
+            return result / numbers.Length;
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Sum(2.5 , 2.5));
+
+            //Console.WriteLine(Calculator.Averange(new double[] { 1,2,3})); без params
+
+            Console.WriteLine(Calculator.Averange(1, 2, 3));
+        }
+    }
+}
